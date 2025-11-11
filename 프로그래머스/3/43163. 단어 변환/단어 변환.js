3f2ced4a0633cbcd8234b71684f97a818ab346ad
Jiyoon <150988439+cjiyun@users.noreply.github.com>
2025-11-11 17:49:08 +0900

    [level 3] Title: 단어 변환, Time: 0.58 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -0,0 +1,27 @@
+function solution(begin, target, words) {
+    const n = words.length;
+    
+    function diff1(a, b) {
+        let diff = 0;
+        for (let i = 0; i < a.length; i++) {
+            if (a[i] !== b[i]) diff++;
+        }
+        return diff === 1;
+    }
+    
+    let q = [[begin, 0]];
+    let head = 0;
+    const visited = Array(n).fill(false);
+    
+    while(head < q.length) {
+        const [cur, dist] = q[head++];
+        if (cur === target) return dist;
+        for (let i = 0; i < n; i++) {
+            if (!visited[i] && diff1(cur, words[i])) {
+                visited[i] = true;
+                q.push([words[i], dist+1]);
+            }
+        }
+    }
+    return 0;
+}
