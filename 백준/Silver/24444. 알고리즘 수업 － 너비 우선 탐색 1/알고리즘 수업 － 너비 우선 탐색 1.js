c674477e6fe55489f254f0474b4b846b6ebf55db
Jiyoon <150988439+cjiyun@users.noreply.github.com>
2025-12-14 15:57:19 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 740 ms, Memory: 98816 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.js
@@ -0,0 +1,34 @@
+const input = require('fs').readFileSync(0, 'utf-8').trim().split('\n');
+const [N, M, R] = input.shift().split(' ').map(Number);
+
+const graph = Array.from(Array(N + 1), () => []);
+for (let i = 0; i < M; i++) {
+  const [u, v] = input[i].split(' ').map(Number);
+  graph[u].push(v);
+  graph[v].push(u);
+}
+for (let i = 1; i <= N; i++) {
+  graph[i].sort((a, b) => a - b);
+}
+
+const visited = Array(N + 1).fill(false);
+const q = [];
+const order = Array(N + 1).fill(0);
+let head = 0, tail = 0;
+let cnt = 1;
+
+const push = v => {
+  q[tail++] = v;
+  visited[v] = true;
+  order[v] = cnt++;
+}
+push(R);
+
+while (head < tail) {
+  const v = q[head++];
+  for (const nv of graph[v]) {
+    if (!visited[nv]) push(nv);
+  }
+}
+
+console.log(order.slice(1).join('\n'));
