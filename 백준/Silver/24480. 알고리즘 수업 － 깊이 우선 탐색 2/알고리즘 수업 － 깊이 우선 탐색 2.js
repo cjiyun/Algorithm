5649949d34bfe82282e80b8ef4e9ee222af03591
Jiyoon <150988439+cjiyun@users.noreply.github.com>
2025-12-22 13:30:43 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 704 ms, Memory: 96212 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.js
@@ -0,0 +1,27 @@
+const input = require('fs').readFileSync(0, 'utf-8').trim().split('\n');
+const [N, M, R] = input.shift().split(' ').map(Number);
+
+const graph = Array.from(Array(N + 1), () => []);
+for (let i = 0; i < M; i++) {
+  const [u, v] = input[i].split(' ').map(Number);
+  graph[u].push(v);
+  graph[v].push(u);
+}
+for (let i = 1; i <= N; i++) {
+  graph[i].sort((a, b) => b - a);
+}
+
+const visited = Array(N + 1).fill(0);
+let order = 1;
+
+const dfs = (v) => {
+  visited[v] = order++;
+  for (const nv of graph[v]) {
+    if (visited[nv] === 0) {
+      dfs(nv);
+    }
+  }
+}
+
+dfs(R)
+console.log(visited.slice(1).join('\n'));
