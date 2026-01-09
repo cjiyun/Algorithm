08a90814185aba56f993b1236986487bab5ec7d8
Jiyoon <150988439+cjiyun@users.noreply.github.com>
2026-01-09 11:50:31 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 6, Time: 648 ms, Memory: 92136 KB -BaekjoonHub

---
+++ 백준/Silver/24484. 알고리즘 수업 － 깊이 우선 탐색 6/알고리즘 수업 － 깊이 우선 탐색 6.js
@@ -0,0 +1,36 @@
+const input = require('fs').readFileSync(0, 'utf-8').trim().split('\n');
+const [N, M, R] = input.shift().split(' ').map(Number);
+
+const adj = Array.from(Array(N + 1), () => []);
+for (let i = 0; i < M; i++) {
+  const [u, v] = input[i].split(' ').map(Number);
+  adj[u].push(v);
+  adj[v].push(u);
+}
+for (let i = 1; i <= N; i++) {
+  adj[i].sort((a, b) => b - a);
+}
+
+const depth = Array(N + 1).fill(-1);
+const order = Array(N + 1).fill(0);
+let t = 1;
+
+const dfs = (v) => {
+  for (const nv of adj[v]) {
+    if (depth[nv] !== -1) continue;
+    depth[nv] = depth[v] + 1;
+    order[nv] = ++t
+    dfs(nv);
+  }
+}
+
+depth[R] = 0;
+order[R] = t;
+dfs(R);
+
+let sum = 0;
+for (let i = 1; i <= N; i++) {
+  sum += depth[i] * order[i];
+}
+
+console.log(sum);
