b856c7cfeea3bc233c927a0367c9e3e814e4200c
Jiyoon <150988439+cjiyun@users.noreply.github.com>
2026-01-31 13:40:07 +0900

    [Silver I] Title: RGB거리, Time: 88 ms, Memory: 9684 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.js
+++ 백준/Silver/1149. RGB거리/RGB거리.js
@@ -4,15 +4,17 @@ const input = require('fs')
   .split('\n')
   .map(line => line.trim().split(' ').map(Number));
 
-const [ N ] = input.shift();
+const [N] = input.shift();
 const costs = [0, ...input];
 
-const dp = Array.from(Array(N + 1), () => Array(3).fill(0));
+let cur = [0, 0, 0];
 
 for (let i = 1; i <= N; i++) {
-  dp[i][0] = costs[i][0] + Math.min(dp[i - 1][1], dp[i - 1][2]);
-  dp[i][1] = costs[i][1] + Math.min(dp[i - 1][0], dp[i - 1][2]);
-  dp[i][2] = costs[i][2] + Math.min(dp[i - 1][0], dp[i - 1][1]);
+  cur = [
+    costs[i][0] + Math.min(cur[1], cur[2]),
+    costs[i][1] + Math.min(cur[0], cur[2]),
+    costs[i][2] + Math.min(cur[0], cur[1])
+  ];
 }
 
-console.log(Math.min(dp[N][0], dp[N][1], dp[N][2]));
+console.log(Math.min(...cur));
