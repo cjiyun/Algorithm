afbaf97a1bdec855278e9a07f0eeacd0b947b87d
Jiyoon <150988439+cjiyun@users.noreply.github.com>
2026-02-11 15:01:06 +0900

    [Gold IV] Title: 테트로미노, Time: 636 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Gold/14500. 테트로미노/테트로미노.js
@@ -0,0 +1,42 @@
+const [[N, M], ...paper] = require('fs')
+  .readFileSync(0, 'utf-8')
+  .trim()
+  .split('\n')
+  .map(line => line.trim().split(' ').map(Number));
+
+const inRange = (r, c) => r >= 0 && r < N && c >= 0 && c < M;
+const visited = Array.from(Array(N), () => Array(M).fill(false));
+const dr = [0, 1, 0, -1], dc = [1, 0, -1, 0];
+let max = 0;
+
+const dfs = (r, c, sum, cnt) => {
+  if (cnt === 4) {
+    max = Math.max(max, sum);
+    return;
+  }
+
+  for (let i = 0; i < 4; i++) {
+    const nr = r + dr[i], nc = c + dc[i];
+    if (!inRange(nr, nc) || visited[nr][nc]) continue;
+
+    if (cnt === 2) {
+      visited[nr][nc] = true;
+      dfs(r, c, sum + paper[nr][nc], cnt + 1);
+      visited[nr][nc] = false;
+    }
+
+    visited[nr][nc] = true;
+    dfs(nr, nc, sum + paper[nr][nc], cnt + 1);
+    visited[nr][nc] = false;
+  }
+}
+
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < M; j++) {
+    visited[i][j] = true;
+    dfs(i, j, paper[i][j], 1);
+    visited[i][j] = false;
+  }
+}
+
+console.log(max);
