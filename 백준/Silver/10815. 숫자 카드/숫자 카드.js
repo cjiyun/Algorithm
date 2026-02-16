61d6f5d175630680ba7e16ee7c312f4df3bf09e1
Jiyoon <150988439+cjiyun@users.noreply.github.com>
2026-02-16 16:10:30 +0900

    [Silver V] Title: 숫자 카드, Time: 656 ms, Memory: 154052 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.js
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.js
@@ -1,20 +1,12 @@
-const input = require('fs')
+let [_N, A, _M, X] = require('fs')
   .readFileSync(0, 'utf-8')
   .trim()
-  .split(/\s+/).map(Number);
+  .split('\n')
+  .map(line => line.trim().split(' ').map(Number));
 
-let p = 0;
-const N = input[p++];
-let A = input.slice(p, p + N);
 A = new Set(A);
 
-p += N;
-const M = input[p++];
-const X = input.slice(p, p + M);
-
 const ans = [];
-for (const x of X) {
-  ans.push(A.has(x) ? 1 : 0);
-}
+for (const x of X) ans.push(A.has(x) ? 1 : 0);
 
 console.log(ans.join('\n'));
