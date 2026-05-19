f01ca0613941ddf24c12b49ef1f183ee0a7968e7
Jiyoon <150988439+cjiyun@users.noreply.github.com>
2026-05-19 18:53:22 +0900

    [level 0] Title: 배열 만들기 2, Time: 259.37 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -1,7 +1,8 @@
 def solution(l, r):
     answer = []
-    
+
     for num in range(l, r + 1):
-        if all(digit in ['0', '5'] for digit in str(num)): answer.append(num)
-    
+        if set(str(num)) <= {'0', '5'}:
+            answer.append(num)
+
     return answer if answer else [-1]
