68ed55173811d681c022ab9c201d20cc0e69152a
Jiyoon <150988439+cjiyun@users.noreply.github.com>
2026-07-07 19:37:20 +0900

    [level 2] Title: 3 x n 타일링, Time: 1.66 ms, Memory: 11 MB -BaekjoonHub

---
+++ 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.py
@@ -0,0 +1,14 @@
+def solution(n):
+    if n % 2 == 1:
+        return 0
+    
+    MOD = 1000000007
+    
+    dp = [0] * (n + 1)
+    dp[0] = 1
+    dp[2] = 3
+    
+    for i in range(4, n + 1, 2):
+        dp[i] = (dp[i - 2] * 4 - dp[i - 4]) % MOD
+    
+    return dp[n]
