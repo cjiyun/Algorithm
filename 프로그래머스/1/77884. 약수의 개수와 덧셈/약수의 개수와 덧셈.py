87d9da1aa24e0af307b8be850ac16a2774011923
Jiyoon <150988439+cjiyun@users.noreply.github.com>
2026-09-03 15:21:31 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.08 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,12 +1,2 @@
 def solution(left, right):
-    cnts = [0] * (right + 1)
-    
-    for i in range(1, right + 1):
-        s = i * (left // i)
-        if left % i != 0:
-            s += i
-        
-        for j in range(s, right + 1, i):
-            cnts[j] += 1
-    
-    return sum(i if x % 2 == 0 else -i for i, x in enumerate(cnts[left:], start=left))
+    return sum(n if (n ** 0.5) % 1 else -n for n in range(left, right + 1))
