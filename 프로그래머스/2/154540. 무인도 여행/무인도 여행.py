da0fdd3236f947d4214060c1a1dc244c28554648
Jiyoon <150988439+cjiyun@users.noreply.github.com>
2026-09-05 09:08:29 +0900

    [level 2] Title: 무인도 여행, Time: 3.85 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
@@ -1,33 +1,33 @@
 from collections import deque
 
 def solution(maps):
+    maps = [list(row) for row in maps]
     n, m = len(maps), len(maps[0])
-    visited = [[False] * m for _ in range(n)]
     moves = [(0, 1), (0, -1), (1, 0), (-1, 0)]
     ans = []
     
     def bfs(sr, sc):
         q = deque([(sr, sc)])
-        visited[sr][sc] = True
         cnt = int(maps[sr][sc])
+        maps[sr][sc] = 'X'
         
         while q:
             r, c = q.popleft()
             for dr, dc in moves:
                 nr, nc = r + dr, c + dc
                 
-                if not (0 <= nr < n and 0 <= nc < m) or maps[nr][nc] == 'X' or visited[nr][nc]:
+                if not (0 <= nr < n and 0 <= nc < m) or maps[nr][nc] == 'X':
                     continue
                 
                 q.append((nr, nc))
-                visited[nr][nc] = True
                 cnt += int(maps[nr][nc])
+                maps[nr][nc] = 'X'
             
         return cnt
     
     for i in range(n):
         for j in range(m):
-            if maps[i][j] == 'X' or visited[i][j]:
+            if maps[i][j] == 'X':
                 continue
             ans.append(bfs(i, j))
     
