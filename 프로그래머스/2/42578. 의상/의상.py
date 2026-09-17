f0d2d0a3a9e9b1c4fed8ab66f760e239cecef9fd
Jiyoon <150988439+cjiyun@users.noreply.github.com>
2026-09-17 17:18:18 +0900

    [level 2] Title: 의상, Time: 0.05 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -0,0 +1,7 @@
+from collections import Counter
+from math import prod
+
+def solution(clothes):
+    cnts = Counter(category for _, category in clothes)
+    
+    return prod([cnt + 1 for cnt in cnts.values()]) - 1
