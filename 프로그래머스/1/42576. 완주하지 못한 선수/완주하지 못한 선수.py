2d7f67685754444a3757a2ee40f6c483a68cdcab
Jiyoon <150988439+cjiyun@users.noreply.github.com>
2026-09-17 16:04:53 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 64.12 ms, Memory: 34.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,14 +1,7 @@
 from collections import Counter
 
 def solution(participant, completion):
-    p_cnt = Counter(participant)
-    c_cnt = Counter(completion)
-    
-    for c, cnt in c_cnt.items():
-         p_cnt[c] -= cnt
-    
-    for p, cnt in p_cnt.items():
-        if cnt > 0:
-            return p
+    ans = Counter(participant) - Counter(completion)
+    return list(ans.keys())[0]
         
     
